12f03ca289c38d39d46f772fcb017e0d1195daab
SepehrNoey <sepehr.nk.81@aut.ac.ir>
2023-01-20 00:46:39 +0330

    feat: BridgeGrid crossing analysis (q8) added.

--- AI_P3/AI_P3/AI_P3/analysis.py
+++ AI_P3/AI_P3/AI_P3/analysis.py
@@ -62,6 +62,7 @@ def question3e():
 def question8():
     answerEpsilon = None
     answerLearningRate = None
+    return 'NOT POSSIBLE'
     return answerEpsilon, answerLearningRate
     # If not possible, return 'NOT POSSIBLE'
 
